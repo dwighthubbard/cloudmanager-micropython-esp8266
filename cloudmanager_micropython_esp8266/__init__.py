3adce4c0826ffd8807e632084e9780bdfdb4f680
Dwight Hubbard <d@dhub.me>
2016-11-09 11:46:35 -0800

    Move common code to module
    Add esp_terminal script to make it easy to get a serial terminal on the device using the same serial port settings as the flasher uses.

--- cloudmanager_micropython_esp8266/__init__.py
+++ cloudmanager_micropython_esp8266/__init__.py
@@ -1,4 +1,18 @@
 import os
+import serial.tools.list_ports
 
 
 FIRMWARE_FILE = os.path.join(os.path.dirname(__file__), 'firmware/firmware-combined.bin')
+KNOWN_SERIAL_DEVICES = ['/dev/ttyUSB0', 'dev/cu.SLAB_USBtoUART', 'COM1']
+
+
+def com_port_devices():
+    return [port.device for port in serial.tools.list_ports.comports()]
+
+
+def determine_default_serial_port():
+    device = KNOWN_SERIAL_DEVICES[0]
+    for device in com_port_devices():
+        if device in KNOWN_SERIAL_DEVICES:
+            return device
+    return device
