a5a52b5547bbf77ad5cf3e9d2d7e2753bf64e4d0
Dwight Hubbard <d@dhub.me>
2016-11-09 16:44:38 -0800

    Change flash_size to detect

--- setup.py
+++ setup.py
@@ -12,7 +12,10 @@ if __name__ == '__main__':
         package_data={
             'cloudmanager_micropython_esp8266': ['firmware/*.bin'],
         },
-        install_requires=['esptool', 'micropython-cloudmanager'],
+        install_requires=[
+            'esptool>1.2.0',
+            'micropython-cloudmanager'
+        ],
         license='MIT',
         long_description=open('README.rst').read(),
         maintainer='Dwight Hubbard',
@@ -20,6 +23,6 @@ if __name__ == '__main__':
         packages=['cloudmanager_micropython_esp8266'],
         scripts=['scripts/flash_esp_image', 'scripts/esp_terminal'],
         url='https://github.com/dwighthubbard/cloudmanager-micropython-esp8266',
-        version='0.0.22',
+        version='0.0.23',
         zip_safe=False,
     )
