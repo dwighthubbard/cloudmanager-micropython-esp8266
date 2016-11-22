2936cb294f3d624896d4407529f604979a588a3e
Dwight Hubbard <d@dhub.me>
2016-11-21 23:34:21 -0800

    Allow faster uploads
    Make it possible to disable services

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
         packages=['cloudmanager_micropython_esp8266'],
         scripts=['scripts/flash_esp_image', 'scripts/esp_terminal'],
         url='https://github.com/dwighthubbard/cloudmanager-micropython-esp8266',
-        version='1.8.6dev4',
+        version='1.8.6dev5',
         zip_safe=False
     )
